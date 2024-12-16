b3740eba2380db4a54986e7a1034bacdfdbeb7db
Stanislaw Sztrajt <stanislawsztrajt@wp.pl>
2024-12-16 16:22:05 +0100

    Added some usage tests for src/index.ts

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,44 @@
-test('should return false after pass wrong command line', () => {
-  
-});
+import { exec } from "child_process";
 
-test('should return true after pass correct command line', () => {
-  
-});
+describe ('Test correct variations of using app', () => {
+  test('Test using app by cli with correct arguments', (done) => {
+    const command = 'npm run sandbox:dev-start container create container-1 image-1'
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Test failed. Error: ${error.message}`)
+        done()
+        return
+      }
+
+      done()
+    })
+  });
+})
+
+describe ('Test wrong variations of using app', () => {
+  test('Test running app in cmd without arguments ', (done) => {
+    const command = 'npm run sandbox:dev-start'
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        done()
+        return
+      }
+
+      console.error(`App should throw error`)
+      done()
+    })
+  });
+
+  test('Test running app in cmd without 1 argument', (done) => {
+    const command = 'npm run sandbox:dev-start container create container-1'
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        done()
+        return
+      }
+
+      console.error(`App should throw error`)
+      done()
+    })
+  });
+})
--- tests/utils/compareTwoArray.test.ts
+++ tests/utils/compareTwoArray.test.ts
@@ -1,25 +1,25 @@
-import { compareTwoArrays } from '../../src/utils/compareTwoArrays';
+import { shallowCompareTwoArrays } from '../../src/utils/shallowCompareTwoArrays';
 
 describe ('testing various correct cases of using function', () => {
   test ('tests comparing length functions', () => {
-    expect(compareTwoArrays([], [''])).toBeFalsy()
+    expect(shallowCompareTwoArrays([], [''])).toBeFalsy()
   })
 
   test ('tests comparing various arrays', () => {
-    expect(compareTwoArrays([1,2,3], ['1', '2', '3'])).toBeFalsy()
+    expect(shallowCompareTwoArrays([1,2,3], ['1', '2', '3'])).toBeFalsy()
   })
 
   test ('tests comparing same arrays', () => {
-    expect(compareTwoArrays([1,2,3], [1,2,3])).toBeTruthy()
+    expect(shallowCompareTwoArrays([1,2,3], [1,2,3])).toBeTruthy()
   })
 })
 
 describe ('testing various wrong cases of using function', () => {
   test ('passing various nested array', () => {
-    expect(compareTwoArrays([[[[[1,2,3]]]]], [[[1,2,3]]])).toBeFalsy()
+    expect(shallowCompareTwoArrays([[[[[1,2,3]]]]], [[[1,2,3]]])).toBeFalsy()
   })
 
   test ('passing same nested array', () => {
-    expect(compareTwoArrays([[[[[1,2,3]]]]], [[[[[1,2,3]]]]])).toBeTruthy()
+    expect(shallowCompareTwoArrays([[[[[1,2,3]]]]], [[[[[1,2,3]]]]])).toBeTruthy()
   })
 })
