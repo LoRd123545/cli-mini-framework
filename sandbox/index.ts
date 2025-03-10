30bdfc20dd0a8e0c37cfdb02dc31f849b25378be
LoRd123545 <kamil.abbasi.official@gmail.com>
2025-03-10 13:28:51 +0100

    added help command

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -5,33 +5,19 @@ const app = new Cli(process.argv.slice(2))
 app
   .scope([])
   .command('version')
-  .arg('arg1')
   .callback((args, opts) => {
-    console.log('version')
+    console.log('v1')
 
     console.log(args, opts)
   })
 
 app
-  .scope([])
-  .command('command1')
-  .arg('arg1')
-  .arg('arg2')
-  .arg('arg3')
+  .scope(['container'])
+  .command('run')
+  .arg('container-name')
+  .option('delete')
   .callback((args, opts) => {
-    console.log('command1')
-
-    console.log(args, opts)
-  })
-
-app
-  .scope([])
-  .command('command2')
-  .arg('arg1')
-  .arg('arg2')
-  .option('opt1')
-  .callback((args, opts) => {
-    console.log('command2')
+    console.log('container run!')
 
     console.log(args, opts)
   })
--- src2/cli.ts
+++ src2/cli.ts
@@ -17,6 +17,7 @@ export class Cli {
   private _scope: string[]
   private _command: string
   private _userPath: string[]
+  private _path: string[]
 
   private _allowedArguments: string[]
   private _arguments: IArgument[]
@@ -36,12 +37,15 @@ export class Cli {
     this._scope = []
     this._command = ''
     this._userPath = []
+    this._path = []
 
     this._allowedArguments = []
     this._arguments = []
 
     this._options = new Map()
     this._allowedOptions = new Set()
+
+    this._allowedOptions.add('help')
   }
 
   private validateAndFillArguments() {
@@ -85,6 +89,28 @@ export class Cli {
     this._options = options
   }
 
+  private help() {
+    console.log('usage: ');
+
+    console.log()
+
+    if(this._allowedArguments.length > 0) {
+      console.log('arguments: ')
+
+      for(const allowedArgument of this._allowedArguments) {
+        console.log(`<${allowedArgument}>`)
+      }
+    }
+
+    if(this._allowedOptions.size > 0) {
+      console.log('options: ')
+
+      for(const [optionName] of this._allowedOptions) {
+        console.log(`<${optionName}>`)
+      }
+    }
+  }
+
   private validate() {
     this.validateAndFillArguments()
     this.validateAndFillOptions()
@@ -107,6 +133,8 @@ export class Cli {
   }
 
   scope(scope: string[]) {
+    this._path = [...scope]
+
     const extractedScope = this._extractor.extractScope(scope.length)
 
     if (compareTwoStringArrays(extractedScope, scope)) {
@@ -119,6 +147,8 @@ export class Cli {
   }
 
   command(commandName: string) {
+    this._path.push(commandName)
+
     const extractedCommand = this._extractor.extractCommand(this._scope.length)
 
     if(!extractedCommand) {
@@ -149,9 +179,7 @@ export class Cli {
   callback(action: (args: IArgument[], opts: Map<string, IOption>) => void) {
     let shouldExecute = true
 
-    const path = [...this._scope, this._command]
-
-    if (!compareTwoStringArrays(path, this._userPath)) {
+    if (!compareTwoStringArrays(this._path, this._userPath)) {
       shouldExecute = false
     }
 
@@ -161,6 +189,10 @@ export class Cli {
     }
 
     this.validate()
+
+    if(this._options.has('help')) {
+      this.help()
+    }
 
     action(this._arguments, this._options)
 
